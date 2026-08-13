744e3ec0149e07f3355323e778cf2748bf6b1957
Abhay Yadav <abhayyadav19955@gmail.com>
2026-08-13 18:24:35 +0530

    Objects

--- JS/Day 8/index.js
+++ JS/Day 8/index.js
@@ -1,5 +1,20 @@
 /*
 Author: Abhay Yadav
-Topic: Arrays in JavaScript
+Topic: Objects in JavaScript
 Date: Thursday, 13 August, 2026 
 */
+
+// In Objects things are stored in Key Value Pair
+
+{
+const user = {
+    name: "Abhay Yadav",
+    age: 19,
+    emailId: "yadav@gmail.com",
+    amount: 3400
+}
+console.log(user);                             // { name: 'Abhay Yadav', age: 19, emailId: 'yadav@gmail.com', amount: 3400 }
+console.log(typeof(user));                     // object
+console.log(user.age);                         // 19
+console.log(user.amount);                      // 3400
+}
