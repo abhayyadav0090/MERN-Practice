ebe606ef41d61ebda4364c3a1b8c275e4ad966b2
Abhay Yadav <abhayyadav19955@gmail.com>
2026-08-14 15:52:03 +0530

    Objects lecture-8

--- JS/Day 8/index.js
+++ JS/Day 8/index.js
@@ -7,14 +7,78 @@ Date: Thursday, 13 August, 2026
 // In Objects things are stored in Key Value Pair
 
 {
-const user = {
-    name: "Abhay Yadav",
-    age: 19,
-    emailId: "yadav@gmail.com",
-    amount: 3400
+    const user = {
+        name: "Abhay Yadav",
+        age: 19,
+        emailId: "yadav@gmail.com",
+        amount: 3400,
+        // home address: Mainpuri                  // error -> You have to use it as string both while creating and accessing
+        // "Home Address": "Mainpuri",             // Correct
+    }
+
+    // console.log(user.name);                     // Abhay Yadav
+    console.log(user["name"]);                     // Abhay Yadav
+    // console.log(user[name]);                    // Error
+
+    console.log(user);                             // { name: 'Abhay Yadav', age: 19, emailId: 'yadav@gmail.com', amount: 3400 }
+    console.log(typeof(user));                     // object
+    // console.log(user.age);                      // 19
+    // console.log(user.amount);                   // 3400
+
+    // Update
+    user.adhaar=1234;
+    user.amount=5000;
+    // console.log(user);                          // { name: 'Abhay Yadav', age: 19, emailId: 'yadav@gmail.com', amount: 5000, adhaar: 1234 }
+
+    // delete                                               
+    delete user.emailId;                                   
+    // console.log(user);                          // { name: 'Abhay Yadav', age: 19, amount: 5000, adhaar: 1234 }
+
+    // Adding and Accessing Multi Word data using String Format
+    console.log(user["Home Address"]);             // Mainpuri -> Accessing  ⭐
+    // console.log(user);                          //  { name: 'Abhay Yadav', age: 19, amount: 5000, adhaar: 1234, 'Home Address': 'Mainpuri' }
+    user["Home Address"]="Mainpuri";               // Adding or Updating     ⭐
+    delete user["Home Address"];                   // Deleting Data
+    console.log(user);                             // { name: 'Abhay Yadav', age: 19, amount: 5000, adhaar: 1234 }
+
+    // Creating a copy of user
+    // ⭐ Objects are copied by Reference -> Change in user2 will be reflected in user;
+    const user2=user;
+    user2.age=90;
+    console.log(user.age);                          // 90
+
+    // ⭐ Accessing Data Inside Objects
+    // console.log(Object.keys(user));              // [ 'name', 'age', 'amount', 'adhaar' ]
+    // console.log(Object.values(user));            // [ 'Abhay Yadav', 90, 5000, 1234 ]
+    // console.log(Object.entries(user));           // [ [ 'name', 'Abhay Yadav' ], [ 'age', 90 ], [ 'amount', 5000 ], [ 'adhaar', 1234 ] ]
+
+    for(let keys in user){
+        console.log(keys, ":", user[keys]);         // name : Abhay Yadav age : 90 amount : 5000 adhaar : 1234
+        // console.log(keys, ":", user.keys);       // Error -> undefined  -->> As No keys was named keys inside user ⭐
+    }
 }
-console.log(user);                             // { name: 'Abhay Yadav', age: 19, emailId: 'yadav@gmail.com', amount: 3400 }
-console.log(typeof(user));                     // object
-console.log(user.age);                         // 19
-console.log(user.amount);                      // 3400
+
+{
+    const user = {
+        name: "Abhay Yadav",
+        age: 19,
+        emailId: "yadav@gmail.com",
+        amount: 3400,
+    }
+
+    // Method 1 ⭐
+    // const name = user.name;
+    // const age = user.age;
+    // console.log(name, age);                         // Abhay Yadav 19
+
+    // Method 2 ⭐
+    const {name, age} = user;
+    console.log(name, age);                            // Abhay Yadav 19
+
+    const arr = [10, 20, 40, 90, 11];
+    const [first, third, random] = arr;                // You can take any name for variable
+    console.log(first, third, random);                 // 10 20 40
+
+    // user.name="Vishnu";
+    // console.log(user);                               // { name: 'Vishnu', age: 19, emailId: 'yadav@gmail.com', amount: 3400 }
 }
